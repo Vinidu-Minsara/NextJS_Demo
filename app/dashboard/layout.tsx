93011ebe5f8482e40112f0cad65dd8fc6aa040a0
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-09-01 14:06:02 +0530

    fix deploy error

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,23 @@
+import React, { ReactNode } from 'react';
 import SideNav from "@/app/ui/dashboard/sidenav";
 
-const DashboardLayout = ({ children }: React.ReactNode) => {
+// Define the props type
+interface DashboardLayoutProps {
+    children: ReactNode;
+}
+
+// Apply the props type to the component
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
-            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+                {children}
+            </div>
         </div>
     );
-}
+};
 
 export default DashboardLayout;
